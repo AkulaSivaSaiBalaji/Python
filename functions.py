3c89c92cbf2abc402ad1f5591ba9da2fb0e6506a
AkulaSivaSaiBalaji <akulasivasaibalaji47@gmail.com>
2026-09-02 15:16:52 +0530

    scope of functions

--- functions.py
+++ functions.py
@@ -85,3 +85,16 @@ def dif(a,b):
     return a-b
 print(dif(10,5))
 '''
+
+for i in range(1,5):
+    for j in range(1,i+1):
+        print('*',end=' ')
+    print()
+'''
+for i in range(0):
+    print('*',end=' ')
+'''
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print('*',end=' ')
+    print()
